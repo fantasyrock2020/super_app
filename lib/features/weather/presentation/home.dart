4aeea48074b152cd89372c1d6214d1abbf0dac5d
sang <fantasyrock2020@gmail.com>
2025-04-17 10:25:30 +0700

    create structure project

---
+++ lib/features/weather/presentation/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeatherHomeScreen extends StatefulWidget {
+  const WeatherHomeScreen({super.key});
+
+  @override
+  State<WeatherHomeScreen> createState() => _WeatherHomeScreenState();
+}
+
+class _WeatherHomeScreenState extends State<WeatherHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
